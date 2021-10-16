53c96e2f3bd23987a6436cc3c9e0eb37a6edbf12
Chapoii <1227339452@qq.com>
2021-10-17 01:22:32 +0800

    update .gitignore

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        // 5位同学的成绩:
+        int[] ns = new int[5];
+        int n = 5;
+        System.out.println(ns); // 索引n不能超出范围
+    }
+}
